68bdde136b272da3b308190782f488fdf47a65e8
Izaak Kuipers <izaak.kuipers@gmail.com>
2024-11-03 19:34:03 +0100

    Don't show the glow on mobile

--- src/routes/MouseGlow.svelte
+++ src/routes/MouseGlow.svelte
@@ -4,6 +4,7 @@
   let show = false;
   let x = 0;
   let y = 0;
+  let render = false;
 
   onMount(() => {
     document.addEventListener("mousemove", (e) => {
@@ -13,7 +14,17 @@
         y = e.clientY;
       }, 0);
     });
+
+    const query = window.matchMedia("(min-width: 750px)");
+
+    query.addEventListener("change", (e) => {
+      render = e.matches;
+    });
+
+    render = query.matches;
   });
 </script>
 
-<div class="mouse-glow" style="--x: {x}px; --y: {y}px" class:show />
+{#if render}
+  <div class="mouse-glow" style="--x: {x}px; --y: {y}px" class:show />
+{/if}
