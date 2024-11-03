d165e1b352cab12386a658d506d76747fca02bcf
Izaak Kuipers <izaak.kuipers@gmail.com>
2024-11-03 19:52:00 +0100

    Add `/en` and `/nl` language endpoints

---
+++ src/routes/en/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  onMount(() => {
+    localStorage.setItem("locale", "en");
+    location.href = "/home";
+  });
+</script>
---
+++ src/routes/nl/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  onMount(() => {
+    localStorage.setItem("locale", "nl");
+    location.href = "/home";
+  });
+</script>
