684afcc0b5575ee62c2d51d02905cf29d0484247
Izaak Kuipers <izaak.kuipers@gmail.com>
2024-11-07 22:34:01 +0100

    Footer

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -9,26 +9,33 @@
 </svelte:head>
 
 <div class="page home">
-  <AnimatedLogo />
-  <h1>
-    <ProgressiveWriter
-      text={[
-        $_("home.hero.top"),
-        { text: "Izaak Kuipers", className: "accent bold" },
-        ".",
-      ]}
-      blinker
-      delay={500}
-    />
-  </h1>
-  <p class="subtitle">
-    <ProgressiveWriter
-      text={[
-        $_("home.hero.bottom.text"),
-        { href: "/about", text: $_("home.hero.bottom.link") },
-      ]}
-      speed={40}
-      delay={2800}
-    />
-  </p>
+  <div class="hero">
+    <AnimatedLogo />
+    <h1>
+      <ProgressiveWriter
+        text={[
+          $_("home.hero.top"),
+          { text: $_("home.hero.name"), className: "accent bold" },
+          ".",
+        ]}
+        blinker
+        delay={500}
+      />
+    </h1>
+    <p class="subtitle">
+      <ProgressiveWriter
+        text={[
+          $_("home.hero.bottom.text"),
+          { href: "/about", text: $_("home.hero.bottom.link") },
+        ]}
+        speed={40}
+        delay={2800}
+      />
+    </p>
+  </div>
+  <footer>
+    &copy;&nbsp;
+    <a href="https://github.com/IzKuipers" target="_blank">{$_("me")}</a>
+    &nbsp;2024
+  </footer>
 </div>
