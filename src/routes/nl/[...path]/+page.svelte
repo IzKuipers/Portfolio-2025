16436b8a3f05836b20a1b71b9c5c596150f9967a
Izaak Kuipers <izaak.kuipers@gmail.com>
2024-12-18 22:59:36 +0100

    Add `/nl/[...path]` for easy Dutch page sharing

---
+++ src/routes/nl/[...path]/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import type { PageData } from "./$types";
+
+  export let data: PageData;
+
+  onMount(() => {
+    localStorage.setItem("locale", "nl");
+    location.href = `/${data.path}`;
+  });
+</script>
