2182a7f6a3e9c0dd29bbcfb0ee235ff651d76cd5
Izaak Kuipers <izaak.kuipers@gmail.com>
2025-02-14 19:02:36 +0100

    /workshop

---
+++ src/routes/workshop/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import ProgressiveWriter from "$lib/ProgressiveWriter.svelte";
+</script>
+
+<div class="page error">
+  <h1>
+    <ProgressiveWriter
+      text={["Under Construction"]}
+      blinker
+      delay={500}
+      speed={100}
+    />
+  </h1>
+  <p class="subtitle">
+    <ProgressiveWriter text={["¯\\_(ツ)_/¯"]} delay={1000} speed={40} />
+  </p>
+</div>
